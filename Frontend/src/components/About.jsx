e93db5d4688ac0bc3ef3efafedca24594525800f
DEVASANJAY002 <152069249+DEVASANJAY002@users.noreply.github.com>
2024-10-01 19:39:02 +0530

    Update About.jsx

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -3,20 +3,12 @@ function About(){
       <section id="about">
         <h1 className="lfh1">About Us</h1>
         <p className="about-desc">
-          Welcome to our online community dedicated to helping you find lost
-          items and reconnect with cherished possessions. At <span >Lost and Found</span>, we
-          understand the heartache and frustration that losing something
-          valuable can bring. Whether it's a beloved pet, a sentimental piece of
-          jewelry, or a vital piece of equipment, the distress of losing an item
-          can be overwhelming. Our mission is simple: to provide a platform
-          where people can share information about lost and found items in
-          public spaces, fostering a sense of community and support. We firmly
-          believe that by coming together, we can increase the chances of
-          reuniting lost items with their rightful owners.
+          This site is intended solely for students to seek help with lost and found items. 
+            Any misuse of this platform for other purposes may result in consequences.
         </p>
         <footer>
             <p>Copyright &copy; 2023</p>
-            <p>Designed and Built by <a href="https://arjuncvinod.me">Arjun C Vinod</a></p>
+            <p>Designed and Built by <a href="#">DAVNS</a></p>
         </footer>
       </section>
       
