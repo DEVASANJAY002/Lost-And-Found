72302c3171e387b141460435294ce83051d7d050
DEVASANJAY002 <152069249+DEVASANJAY002@users.noreply.github.com>
2024-10-01 21:01:53 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,6 @@
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://devasanjay14:<db_password>@cluster0.qcfzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://devasanjay14:<devasanjay14>@cluster0.qcfzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
