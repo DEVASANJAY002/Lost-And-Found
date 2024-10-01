730ae39a68c850385ca352a08a0c42d2c8591f40
DEVASANJAY002 <152069249+DEVASANJAY002@users.noreply.github.com>
2024-10-01 20:58:49 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,119 +1,26 @@
-import express, { request } from "express";
-import { PORT, mongoURL } from "./config.js";
-import mongoose from "mongoose";
-import { Item } from "./models/itemmodel.js";
-import cors from "cors";
-import { createRequire } from "module";
-const require = createRequire(import.meta.url);//to require require for multer
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://devasanjay14:<db_password>@cluster0.qcfzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use('/files',express.static("files"))
-
-//================================================== multer ==============================================
-
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "./files");
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now();
-    cb(null,uniqueSuffix+file.originalname);
-  },
-});
-
-const upload = multer({ storage: storage });
-
-
-// ============================== get =================================
-
-app.get("/item", async (req, res) => {
-  try {
-    const items = await Item.find({});
-    return res.status(200).json({
-      count: items.length,
-      data: items,
-    });
-  } catch (error) {
-    res.status(500).send({ message: error.message });
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
   }
 });
 
-// ===============================post===================================
-
-app.post("/item",upload.single("file"), async (req,res)=>{
-  console.log(req.file);
+async function run() {
   try {
-    if (
-      !req.body.name ||
-      !req.body.email ||
-      !req.body.phoneno ||
-      !req.body.title ||
-      !req.body.description
-    ) {
-      return res.status(400).send({ message: "all fields sent" });
-    }
-
-   const newItem = {
-      name: req.body.name,
-      email: req.body.email,
-      phoneno: req.body.phoneno,
-      title: req.body.title,
-      description: req.body.description,
-      image: req.file.filename,
-    };
-   const item=await Item.create(newItem);
-   return res.status(200).send(item);
-
-  }catch(error){
-    console.log(error);
-    res.status(500).send("error");
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
   }
-
-})
-
-
-// =================================-get id ==================================
-
-app.get("/item/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const item = await Item.findById(id);
-    return res.status(200).json(item);
-  } catch (error) {
-    res.status(500).send({ message: error.message });
-  }
-});
-
-// =================================== delete ============================
-
-app.delete("/item/:id", async (req, res) => {
-  try {
-    const id = req.params.id;
-    const result = await Item.findByIdAndDelete(id);
-    if (!result) {
-      return res.status(404).send({ message: "Item not found" });
-    }
-    return res.status(200).send({ message: "Item deleted" });
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).send({ message: error.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`server started at port ${PORT}`);
-});
-
-mongoose
-  .connect(mongoURL)
-  .then(() => {
-    console.log("Connected to database");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+}
+run().catch(console.dir);
