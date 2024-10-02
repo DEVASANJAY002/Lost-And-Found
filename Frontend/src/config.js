0dd1fbdbd8218c4e150dc0afbe47ab8140bfaa73
DEVASANJAY002 <152069249+DEVASANJAY002@users.noreply.github.com>
2024-10-02 16:46:09 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,5 +1,5 @@
-export const api = "https://dull-gold-clam-tutu.cyclic.app"
+// export const api = "https://dull-gold-clam-tutu.cyclic.app"
 
 
-export const api = "http://localhost:8000";
+// export const api = "http://localhost:8000";
 export const api = "https://lost-and-found-sec.vercel.app/find";
