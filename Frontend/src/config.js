178b6d8ba26c326e17d03292d51d6f4e367bae94
DEVASANJAY002 <152069249+DEVASANJAY002@users.noreply.github.com>
2024-10-02 16:42:55 +0530

    Update config.js

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,4 +1,5 @@
 // export const api = "https://dull-gold-clam-tutu.cyclic.app"
-export const api = "https://lostandfound-api.onrender.com";
+export const mongoURL = "mongodb+srv://devasanjay14:<devasanjay14>@cluster0.qcfzr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
 // export const api = "http://localhost:8000";
 // export const api = "https://lost-and-found-sec.vercel.app/find";
